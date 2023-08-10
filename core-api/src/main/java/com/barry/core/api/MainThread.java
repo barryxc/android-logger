0c7d866ed02adff433e29d8a040b57e1358a2610
云繁 <xuechuang.xc@alibaba-inc.com>
2023-08-10 15:31:22 +0800

    feat(logger): support for mainthread

---
+++ core-api/src/main/java/com/barry/core/api/MainThread.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) barry 2023 - 2023, All Rights Reserved.
+ */
+
+package com.barry.core.api;
+
+/**
+ * @author yunfan
+ * @since 2023/8/10
+ */
+public @interface MainThread {
+}
