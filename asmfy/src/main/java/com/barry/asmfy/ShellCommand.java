38968570816c22e29595509d4695ebebadd722c3
云繁 <xuechuang.xc@alibaba-inc.com>
2023-08-12 14:20:35 +0800

    feat(logger): update copyright

--- asmfy/src/main/java/com/barry/asmfy/ShellCommand.java
+++ asmfy/src/main/java/com/barry/asmfy/ShellCommand.java
@@ -1,5 +1,17 @@
 /*
- * Copyright (c) barry 2023 - 2023, All Rights Reserved.
+ * Copyright barry 2023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.barry.asmfy;
@@ -18,7 +30,7 @@ import picocli.CommandLine;
 @CommandLine.Command(name = "asmfy", description = "generate bytecode")
 public class ShellCommand implements Callable {
 
-    @CommandLine.Option(names = {"-i", "--input"}, description = "input file")
+    @CommandLine.Option(names = {"-i", "--input"}, description = "input .class file")
     public String path;
 
 
